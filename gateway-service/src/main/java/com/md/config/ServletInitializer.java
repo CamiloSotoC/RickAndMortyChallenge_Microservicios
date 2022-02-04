e09be01a96ceecd27cfb8653699a3aab73454977
camilo.soto <camilo.soto@usach.cl>
2022-02-04 18:12:40 -0300

    feat: gateway-service is added

---
+++ gateway-service/src/main/java/com/md/config/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.md.config;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GatewayServiceApplication.class);
+	}
+
+}
